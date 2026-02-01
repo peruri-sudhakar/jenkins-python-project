618d8fed342fe89f13ac9162884763fc7b24b0bc
Sudhakar Peruri <sudhakar.p89@yahoo.com>
2026-02-01 07:37:47 +0530

    Webhook test change

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
 import logging
+import socket
 
-# Configure logging
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
+logging.info("Webhook test: new change pushed")
+
 def add(a, b):
     logging.info(f"Adding {a} and {b}")
-    result = a + b
-    logging.info(f"Result is {result}")
-    return result
+    return a + b
 
 if __name__ == "__main__":
-    logging.info("Application started")
     add(10, 20)
-    logging.info("Application finished")
